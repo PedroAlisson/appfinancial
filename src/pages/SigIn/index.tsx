6d72f80ff3337643e5e8b5df943c58a8afe41f5a
PedroAlisson <pedrodevelope@gmail.com>
2021-05-15 08:00:24 -0300

    Estructura Project

---
+++ src/pages/SigIn/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import {} from "react-native";
+
+import { Container } from "./styles";
+const SigIn: React.FC = () => {
+  return <Container></Container>;
+};
+
+export default SigIn;
---
+++ src/pages/SigUp/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import {} from "react-native";
+import "./styles";
+
+import { Container } from "./styles";
+const SigUp: React.FC = () => <Container></Container>;
+
+export default SigUp;
