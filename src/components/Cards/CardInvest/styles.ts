e943704281c516fe508282de8aeac64e23cb5a99
PedroAlisson <pedrodevelope@gmail.com>
2021-06-20 19:59:45 -0300

    add color and cardinvest

--- src/components/Cards/CardInvest/styles.ts
+++ src/components/Cards/CardInvest/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 export const SelectView = styled.View`
   border-color: gray;
   border-radius: 30px;
-  background-color: #fff;
+  background-color: gray;
   width: 95%;
   padding: 20px;
   margin-bottom: 15px;
@@ -13,6 +13,9 @@ export const SelectView = styled.View`
 export const ViewContainer = styled.View`
   align-items: center;
   justify-content: center;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 10px;
 `;
 
 export const TextInvest = styled.Text`
